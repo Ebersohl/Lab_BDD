689a619f59bddd6a0efdad0f2162edd0724a1059
Ebersohl <51333216+Ebersohl@users.noreply.github.com>
2019-12-03 16:40:57 -0500

    Update test_duckduckgo_steps.py

--- tests/step_defs/test_duckduckgo_steps.py
+++ tests/step_defs/test_duckduckgo_steps.py
@@ -8,11 +8,14 @@
 #  ---------------------------------------------------------------------------
 
 import requests
-from pytest_bdd import scenario, given, then, parsers
+from pytest_bdd import scenario, given, then
 
 DUCKDUCKGO_API = 'https://api.duckduckgo.com'
 
+
 @scenario('../features/duckduckgo.feature', 'Presidents in Related Topics')
+def test_start():
+    pass
 
 
 @given('the DuckDuckGo API is queried with "presidents of the united states"')
